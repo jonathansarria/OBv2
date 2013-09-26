1f8370981710cdba10e50bf4d74764599bb7a3b0
Jonathan Sarria <jsarria.pb@gmail.com>
2013-09-26 13:08:49 -0700

    Database select fix

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,5 +1,5 @@
 class Schedule < ActiveRecord::Base
   def Schedule.available
-    self.select("title, course, beg_time, end_time").where.not({status: "Closed", status: "Cancelled"})
+    self.select("title, course, beg_time, end_time").where.not(status: ["Closed", "Cancelled"])
   end
 end
