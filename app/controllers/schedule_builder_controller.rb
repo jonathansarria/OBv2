ef581189971760974626faa11078748a9ae3c986
Jonathan Sarria <jsarria.pb@gmail.com>
2013-09-27 02:23:46 -0700

    Minimize week time range

--- app/controllers/schedule_builder_controller.rb
+++ app/controllers/schedule_builder_controller.rb
@@ -1,5 +1,66 @@
 class ScheduleBuilderController < ApplicationController
   def index
   	@courses = Schedule.available
+    @earliest = 8
+    @latest = 20
+
+    get_min_times
   end
+
+  def get_min_times
+    # Set minimum hour range for week table
+    times = Schedule.times
+    unless times[0].nil? && times[1].nil?
+      if times[1].nil?
+        @earliest = times[0][:first][:beg_time][/^[^:]*/].to_i
+        @latest = times[0][:last][:beg_time][/^[^:]*/].to_i
+      elsif times[0].nil?
+        @earliest = times[1][:first][:beg_time][/^[^:]*/].to_i + 12
+        @latest = times[1][:last][:beg_time][/^[^:]*/].to_i + 12
+      else
+        @earliest = times[0][:first][:beg_time][/^[^:]*/].to_i
+        @latest = times[1][:last][:beg_time][/^[^:]*/].to_i + 12
+      end
+    end
+  end
+
+=begin
+  def get_times
+    cont = {am: true, pm: true}
+    early = Hash.new(nil)
+    late = Hash.new(nil)
+
+    Schedule.times.each do |time|
+      if time[:beg_time].match(/AM/)
+        if cont[:am] || (time[:beg_time].match(/^12/))
+          early[:first] = time[:beg_time]
+          cont[:am] = false
+        elsif !(time[:beg_time].match(/^12/))
+          early[:last] = time[:beg_time]
+        end
+      end
+      if time[:beg_time].match(/PM/) 
+        if cont[:pm] || (time[:beg_time].match(/^12/))
+          late[:first] = time[:beg_time]
+          cont[:pm] = false
+        elsif !(time[:beg_time].match(/^12/))
+          late[:last] = time[:beg_time]
+        end
+      end
+    end
+      
+    unless early.nil? && late.nil?
+      if late.nil?
+        @earliest = early[:first][/^[^:]*/].to_i
+        @latest = early[:last][/^[^:]*/].to_i
+      elsif early.nil?
+        @earliest = late[:first][/^[^:]*/].to_i
+        @latest = late[:last][/^[^:]*/].to_i
+      else
+        @earliest = early[:first][/^[^:]*/].to_i
+        @latest = late[:last][/^[^:]*/].to_i + 12
+      end
+    end
+  end
+=end
 end
--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,5 +1,30 @@
 class Schedule < ActiveRecord::Base
   def Schedule.available
-    self.select("title, course, beg_time, end_time").where.not(status: ["Closed", "Cancelled"])
+    self.where.not(status: ["Closed", "Cancelled"]).order(:title)
+  end
+
+  def Schedule.times
+    earliest = Hash.new(nil)
+    latest = Hash.new(nil)
+
+    stmnt = self.select(:beg_time).where('beg_time LIKE "%AM"').order(:beg_time)
+    if self.where('beg_time LIKE "12%AM"').any?
+      earliest[:first] = stmnt.where('beg_time LIKE "12%"').first
+      earliest[:last] = stmnt.where('NOT (beg_time LIKE "12%")').last
+    else
+      earliest[:first] = stmnt.first
+      earliest[:last] = stmnt.last
+    end
+    
+    stmnt = self.select(:beg_time).where('beg_time LIKE "%PM"').order(:beg_time)
+    if self.where('beg_time LIKE "12%PM"').any?
+      latest[:first] = stmnt.where('beg_time LIKE "12%"').first
+      latest[:last] = stmnt.where('NOT (beg_time LIKE "12%")').last
+    else
+      latest[:first] = stmnt.first
+      latest[:last] = stmnt.last
+    end
+    
+    return [earliest, latest]
   end
 end
